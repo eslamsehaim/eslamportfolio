887d625a53e8a6165a901a0e30c62864e997a783
eslam.alaa.sehaim <eslam.alaa.sehaim@outlook.com>
2026-02-06 00:25:03 +0200

    p6

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,34 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('eslamportfolio');
+
+  LightModeFlag: boolean = false;
+  DarkModeFlag: boolean = true;
+
+  LightMode() {
+    this.LightModeFlag = false;
+    this.DarkModeFlag = true;
+  }
+
+  DarkMode() {
+    this.LightModeFlag = true;
+    this.DarkModeFlag = false;
+  }
+
+  openProfile() {
+    // $('#PicModal').modal('show');
+  }
+
+  closeProfile() {
+    // $('#PicModal').modal('hide');
+  }
 }
