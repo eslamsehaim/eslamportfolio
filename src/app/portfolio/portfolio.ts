aca513bfd2a5280eb5797ffa367b58d5a2d06af0
eslam.alaa.sehaim <eslam.alaa.sehaim@outlook.com>
2026-02-06 04:56:19 +0200

    p9

--- src/app/portfolio/portfolio.ts
+++ src/app/portfolio/portfolio.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './portfolio.css',
 })
 export class Portfolio {
+  openLinkedin() {
+    window.open('https://www.linkedin.com/in/eslam-mohammed-alaa-717156101/', '_blank', 'noopener,noreferrer');
+  }
 
+  openProjects() {
+    // $('#ProjectsModal').modal('show');
+  }
+
+  closeProjects() {
+    // $('#ProjectsModal').modal('hide');
+  }
 }
